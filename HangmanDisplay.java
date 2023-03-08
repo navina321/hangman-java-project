43c54f34723001ea072ae79efe821ee3a7959e86
navina321 <81630548+navina321@users.noreply.github.com>
2023-03-08 14:51:43 +0000

    added Hangman display class

---
+++ HangmanDisplay.java
@@ -0,0 +1,73 @@
+public class HangmanDisplay {
+    public void drawHangman(){
+        int livesLeft = 8;
+        if(livesLeft == 8){
+            System.out.println("   |----¬");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        else if(livesLeft ==7){
+            System.out.println("   |---¬");
+            System.out.println("   |   O");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        else if(livesLeft ==6){
+            System.out.println("   |----¬");
+            System.out.println("   |    O");
+            System.out.println("   |    |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        else if(livesLeft ==5){
+            System.out.println("   |----¬");
+            System.out.println("   |    O");
+            System.out.println("   |   (|");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }else if(livesLeft ==4){
+            System.out.println("   |----¬");
+            System.out.println("   |    O");
+            System.out.println("   |   (|)");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }else if(livesLeft ==3){
+            System.out.println("   |----¬");
+            System.out.println("   |    O");
+            System.out.println("   |   (|)");
+            System.out.println("   |   /");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }else if(livesLeft ==2){
+            System.out.println("   |----¬");
+            System.out.println("   |    O");
+            System.out.println("   |   (|)");
+            System.out.println("   |   /|");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        else if(livesLeft ==1){
+            System.out.println("   |---¬");
+            System.out.println("   |   O");
+            System.out.println("   |  (|)");
+            System.out.println("   | _/|");
+            System.out.println("   |");
+            System.out.println("___|___");
+        } else {
+            System.out.println("   |----¬");
+            System.out.println("   |    O");
+            System.out.println("   |   (|)");
+            System.out.println("   |  _/|_");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+    }
+}
